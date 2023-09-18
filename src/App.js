b1978d8430ba9f072e79be2e515fc3fa12242a48
M <mrgmckenzie@gmail.com>
2023-09-18 12:44:08 +0100

    refactored to remove old code and created parameterForm component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import { useState } from 'react';
 import './App.css';
 import { useEffect } from 'react';
-import { eventWrapper } from '@testing-library/user-event/dist/utils';
+import ParameterForm from './components/form';
 
 function App() {
 
   const [dot, setDot] = useState({})
-  // const [dots, setDots] = useState([])
   const [totalDots, setTotalDots] = useState(15000)
   const [canvasHeight, setCanvasHeight] = useState(600)
   const [canvasWidth, setCanvasWidth] = useState(800)
-  const [brushStrokeLength, setBrushStrokeLength] = useState(100)
-  const [diameter, setDiameter] = useState(22)
-  const [directionBeforeChange, setDirectionBeforeChange] = useState(2)
-  const [opacity, setOpacity] = useState(80)
+  const [brushStrokeLength, setBrushStrokeLength] = useState(200)
+  const [diameter, setDiameter] = useState(30)
+  const [directionBeforeChange, setDirectionBeforeChange] = useState(1)
+  const [opacity, setOpacity] = useState(85)
   const [run, setRun] = useState([])
-  const [speed, setSpeed] = useState(3)
+  const [speed, setSpeed] = useState(15)
 
-
-  const dotArray = []
-
-  let maxHue = Math.floor( Math.random()*360)
+  let maxHue = Math.floor(Math.random() * 360)
   let minHue = maxHue - 10
 
   let maxLight = 65
@@ -28,50 +24,10 @@ function App() {
 
   const randomMaxlight = Math.floor(maxLight)
   const randomMinlight = Math.ceil(minLight)
-  const randomlightness = Math.floor( Math.random() * (randomMaxlight - randomMinlight) + randomMinlight)
+  const randomlightness = Math.floor(Math.random() * (randomMaxlight - randomMinlight) + randomMinlight)
 
   const minTop = 0
-  //const canvasHeight = 500
-
   const minLeft = 0
-  //const canvasWidth = 500
-
-  //const totalDots = 100000
-  
-  const handleTotalDotChange = (event) => {
-    setTotalDots(event.target.value)
-  };
-  
-  const handleCanvasHeightChange = (event) => {
-    setCanvasHeight(event.target.value)
-  };
-  
-  const handleCanvasWidthChange = (event) => {
-    setCanvasWidth(event.target.value)
-  };
-
-  const handleSubmit = (event) => {
-      event.preventDefault();
-      let runAgain = [...run]
-      setRun([runAgain[0] + 1]);
-  }
-  
-  const handleBrushStrokeLengthChange = (event) => {
-    setBrushStrokeLength(event.target.value)
-  };
-  const handleDiameterChange = (event) => {
-    setDiameter(event.target.value)
-  };
-  const handleOpacityChange = (event) => {
-    setOpacity(event.target.value)
-  };
-  const handleDirectionBeforeChange = (event) => {
-    setDirectionBeforeChange(event.target.value)
-  };
-  const handleSpeedChange = (event) => {
-    setSpeed(event.target.value)
-  };
-  //const brushStrokeLength = 1000
 
   let dotCount = 0
   let top = 25
@@ -83,137 +39,118 @@ function App() {
 
     const interval = setInterval(() => {
 
-      if (dotCount%directionBeforeChange === 0) {
+      if (dotCount % directionBeforeChange === 0) {
         if (randomDirection === 0) {
-          randomDirection = [0,4,7][Math.floor(Math.random() * 3)]
+          randomDirection = [0, 4, 7][Math.floor(Math.random() * 3)]
         } else if (randomDirection === 1) {
-          randomDirection = [1, 5, 6][Math.floor(Math.random() * 3)]          
-        } else if (randomDirection === 2){
-        randomDirection = [2, 4, 5][Math.floor(Math.random() * 3)] 
-        } else if (randomDirection === 3){
-        randomDirection = [3, 6, 7][Math.floor(Math.random() * 3)] 
+          randomDirection = [1, 5, 6][Math.floor(Math.random() * 3)]
+        } else if (randomDirection === 2) {
+          randomDirection = [2, 4, 5][Math.floor(Math.random() * 3)]
+        } else if (randomDirection === 3) {
+          randomDirection = [3, 6, 7][Math.floor(Math.random() * 3)]
         } else if (randomDirection === 4) {
-        randomDirection = [4, 0, 2][Math.floor(Math.random() * 3)] 
+          randomDirection = [4, 0, 2][Math.floor(Math.random() * 3)]
         } else if (randomDirection === 5) {
-        randomDirection = [5, 1, 2][Math.floor(Math.random() * 3)] 
+          randomDirection = [5, 1, 2][Math.floor(Math.random() * 3)]
         } else if (randomDirection === 6) {
-        randomDirection = [6, 1, 3][Math.floor(Math.random() * 3)] 
+          randomDirection = [6, 1, 3][Math.floor(Math.random() * 3)]
         } else if (randomDirection === 7) {
-        randomDirection = [7, 0, 3][Math.floor(Math.random() * 3)] 
-      } else {
-        Math.floor(Math.random() * 8)
+          randomDirection = [7, 0, 3][Math.floor(Math.random() * 3)]
+        } else {
+          Math.floor(Math.random() * 8)
+        }
       }
-    }
-      
 
+      if (dotCount % brushStrokeLength === 0) {
 
-      if (dotCount%brushStrokeLength === 0) {
-        
-        top = Math.floor(Math.random()*canvasHeight)
-        left = Math.floor(Math.random()*canvasWidth)
-
-        // if (Math.floor(Math.random()*2) === 0) {
-        // maxHue = maxHue +1
-        // minHue = minHue +1
-        // } else {
-        //   maxHue = maxHue -1
-        //   minHue = minHue -1
-        // }
+        top = Math.floor(Math.random() * canvasHeight)
+        left = Math.floor(Math.random() * canvasWidth)
 
         //TODO add in transition to fade colours
-
       }
 
-      let min = Math.ceil(diameter-diameter/5);
+      let min = Math.ceil(diameter - diameter / 5);
       let max = Math.floor(diameter);
-      let randomDiameter = Math.floor((Math.random()*(max-min)) + min)
+      let randomDiameter = Math.floor((Math.random() * (max - min)) + min)
 
-        if (randomDirection === 0) {
-          top = top+randomDiameter/3
-        } else if (randomDirection === 1) {
-          top = top-randomDiameter/3
-        } else if (randomDirection === 2) {
-          left = left+randomDiameter/3
-        } else if (randomDirection === 3) {
-          left = left-randomDiameter/3
-        } else if (randomDirection === 4) {
-          top = top+randomDiameter/3
-          left = left+randomDiameter/3
-        } else if (randomDirection === 5) {
-          top = top-randomDiameter/3
-          left = left+randomDiameter/3
-        } else if (randomDirection === 6) {
-          top = top-randomDiameter/3
-          left = left-randomDiameter/3
-        } else {
-          top = top+randomDiameter/3
-          left = left-randomDiameter/3
-        }
+      if (randomDirection === 0) {
+        top = top + randomDiameter / 3
+      } else if (randomDirection === 1) {
+        top = top - randomDiameter / 3
+      } else if (randomDirection === 2) {
+        left = left + randomDiameter / 3
+      } else if (randomDirection === 3) {
+        left = left - randomDiameter / 3
+      } else if (randomDirection === 4) {
+        top = top + randomDiameter / 3
+        left = left + randomDiameter / 3
+      } else if (randomDirection === 5) {
+        top = top - randomDiameter / 3
+        left = left + randomDiameter / 3
+      } else if (randomDirection === 6) {
+        top = top - randomDiameter / 3
+        left = left - randomDiameter / 3
+      } else {
+        top = top + randomDiameter / 3
+        left = left - randomDiameter / 3
+      }
 
-        if (top >= canvasHeight) {
-          top = 1
-        }
-        if (top <= minTop) {
-          top = canvasHeight
-        }
-        if (left >= canvasWidth) {
-          left = 1
-        }
-        if (left <= minLeft) {
-          left = canvasWidth
-        }
-      
+      if (top >= canvasHeight) {
+        top = 1
+      }
+      if (top <= minTop) {
+        top = canvasHeight
+      }
+      if (left >= canvasWidth) {
+        left = 1
+      }
+      if (left <= minLeft) {
+        left = canvasWidth
+      }
 
       const randomMaxHue = Math.floor(maxHue)
       const randomMinHue = Math.ceil(minHue)
-      const randomHue = Math.floor( Math.random() * (randomMaxHue-randomMinHue) + randomMinHue)
-  
+      const randomHue = Math.floor(Math.random() * (randomMaxHue - randomMinHue) + randomMinHue)
+
       const maxSat = Math.floor(100);
       const minSat = Math.ceil(65);
-      const randomSaturation = Math.floor( Math.random()*(maxSat-minSat) + minSat)
+      const randomSaturation = Math.floor(Math.random() * (maxSat - minSat) + minSat)
 
+      if (Math.floor(Math.random() * 2) === 0) {
+        maxHue = maxHue + 0.1
+        minHue = minHue + 0.1
+      } else {
+        maxHue = maxHue - 0.1
+        minHue = minHue - 0.1
+      }
 
-        if (Math.floor(Math.random()*2) === 0) {
-        maxHue = maxHue +0.2
-        minHue = minHue +0.2
-        } else {
-          maxHue = maxHue -0.2
-          minHue = minHue -0.2
-        }
-      
-      setDot( {index: dotCount, hue: randomHue, saturation: randomSaturation, lightness: randomlightness, top: top, left: left, diameter: randomDiameter} )
+      setDot({ index: dotCount, hue: randomHue, saturation: randomSaturation, lightness: randomlightness, top: top, left: left, diameter: randomDiameter })
 
       //add to dot count
       dotCount++
-
 
       //end the loop
       if (dotCount >= totalDots) {
         clearInterval(interval)
       }
-
     }
       //speed of dot generation
-    ,speed);
+      , speed);
 
     return () => clearInterval(interval);
 
   }, [run]);
 
-  // useEffect(() => {
-  //   setDots([...dots, dot])
-  // }, [dot])
+  let canvas;
+  let context;
 
-let canvas;
-let context;
-
-//set canvas background to white
-useEffect(() => {
-  canvas = document.getElementById('dotCanvas')   // access the canvas object
-  context = canvas.getContext('2d')                // set context to 2d
-  context.fillStyle = 'whitesmoke'
-  context.fillRect(0,0, canvasWidth, canvasHeight)
-}, [canvasHeight, canvasWidth, run])
+  //set canvas background to white
+  useEffect(() => {
+    canvas = document.getElementById('dotCanvas')   // access the canvas object
+    context = canvas.getContext('2d')                // set context to 2d
+    context.fillStyle = 'whitesmoke'
+    context.fillRect(0, 0, canvasWidth, canvasHeight)
+  }, [canvasHeight, canvasWidth, run])
 
   useEffect(() => {
     canvas = document.getElementById('dotCanvas')   // access the canvas object
@@ -221,7 +158,7 @@ useEffect(() => {
 
     let blob = new Path2D();
     //TO DO - make it that dot is centred on the line and does not generate with overhang below or to right
-    blob.roundRect(dot.left, dot.top, dot.diameter, dot.diameter, dot.diameter/5)
+    blob.roundRect(dot.left, dot.top, dot.diameter, dot.diameter, dot.diameter / 5)
 
     context.fillStyle = `hsl(${dot.hue} ${dot.saturation}% ${dot.lightness}% / ${opacity}%)`
     context.fill(blob)
@@ -229,44 +166,12 @@ useEffect(() => {
   }, [dot, run])
 
 
-
   return (
     <div className="App">
-      {/* <div id='canvas'>
-          {[...dots.map( dot => {
-            return <div className='dot' key={dot.key} style={{background: `hsl(${dot.hue} ${dot.saturation}% ${dot.lightness}%)`, top: dot.top, left: dot.left, width: dot.diameter, height: dot.diameter}}></div>
-          })]} */}
-          <canvas id="dotCanvas" width={canvasWidth} height={canvasHeight}>
-  UPDATE BROWSER PLEASE!
-            </canvas>
-            <form onSubmit={handleSubmit}>
-              <label>Total # of Dots:
-                <input onChange={handleTotalDotChange} min={1000} value={totalDots} type='number'></input>
-              </label>
-              <label>Brush Stroke Length:
-                <input onChange={handleBrushStrokeLengthChange} value={brushStrokeLength} type='number'></input>
-              </label>
-              <label>Change Direction (px):
-                <input onChange={handleDirectionBeforeChange} min={1} value={directionBeforeChange} type='number'></input>
-              </label>
-              <label>Diameter (px):
-                <input onChange={handleDiameterChange} value={diameter} type='number'></input>
-              </label>
-              <label>Opacity (%):
-                <input onChange={handleOpacityChange} min={0} max={100} value={opacity} type='number'></input>
-              </label>
-              <label>Canvas Height (pixels):
-                <input onChange={handleCanvasHeightChange} min={100} max={700} value={canvasHeight} type='number'></input>
-              </label>
-              <label>Canvas Width (pixels):
-                <input onChange={handleCanvasWidthChange} min={100} max={1450} value={canvasWidth} type='number'></input>
-              </label>
-              <label>Speed of dot generation (ms):
-                <input onChange={handleSpeedChange} min={0.1} max={1000} value={speed} type='number' step={0.1}></input>
-              </label>
-              <input type="submit" value="ART!" />
-            </form>
-        {/* </div> */}
+      <canvas id="dotCanvas" width={canvasWidth} height={canvasHeight}>
+        Please upgrade your browser
+      </canvas>
+      <ParameterForm totalDots={totalDots} setTotalDots={setTotalDots} canvasHeight={canvasHeight} setCanvasHeight={setCanvasHeight} canvasWidth={canvasWidth} setCanvasWidth={setCanvasWidth} brushStrokeLength={brushStrokeLength} setBrushStrokeLength={setBrushStrokeLength} diameter={diameter} setDiameter={setDiameter} directionBeforeChange={directionBeforeChange} setDirectionBeforeChange={setDirectionBeforeChange} opacity={opacity} setOpacity={setOpacity} speed={speed} setSpeed={setSpeed} run={run} setRun={setRun}/>
     </div>
   );
 }
--- src/components/form.js
+++ src/components/form.js
@@ -1,265 +1,66 @@
-import { useState } from "react"
+const ParameterForm = ({totalDots, setTotalDots, canvasHeight, setCanvasHeight, canvasWidth, setCanvasWidth, brushStrokeLength, setBrushStrokeLength, diameter, setDiameter, directionBeforeChange, setDirectionBeforeChange, opacity, setOpacity, speed, setSpeed, run, setRun}) => {
 
-
-const FormContainer = ({geoList, setGeoList, setGeoObj,runForecast, setSavedSearchList}) => {
-
-    const rightHereRightNow = new Date().toISOString().slice(0, 10)
-
-    const [search, setSearch] = useState({
-        departureString: "",
-        departureDate: "",
-        departureTime: "",
-        arrivalString: "",
-        arrivalDate: "",
-        arrivalTime: ""
-    })
-
-    const [saveSearchChecked, setSaveSearchChecked] = useState(false)
-
-    const [departureGeoList, setDepartureGeoList] = useState("")
-    const [arrivalGeoList, setArrivalGeoList] = useState("")
-
-    const [selectedDepartureLocation, setSelectedDepartureLocation] = useState({
-        departureLatitude: "",
-        departureLongitude: "",
-    })
-    console.log(selectedDepartureLocation)
-    const [selectedArrivalLocation, setSelectedArrivalLocation] = useState({
-        arrivalLatitude: "",
-        arrivalLongitude: "",
-    })
-    
- 
-    
-    const mapFunction = (inputGeoList) => {
-    let resultOfMap
-        if (inputGeoList && inputGeoList.length > 0) {
-            resultOfMap = inputGeoList.map( geoLocation => {
-                const opt = `${geoLocation.name} - ${geoLocation.country} ${ (geoLocation.admin1 ? "- "  + geoLocation.admin1 :false || geoLocation.admin2 ? "- "  + geoLocation.admin2 :false)  || " "}` // consider adding a helper function, as same logic to the left can be needed for edge case when "country" is undefined nice api :)
-                return <option key={geoLocation.id} value={geoLocation.id} > {opt} </option> 
-        })}
-
-    return resultOfMap}
-    // onClick={()=>handleClick(geoLocation)}
-
-    // let parsedDepartureGeoList;
-    // let parsedArrivalGeoList;
-    const parsedDepartureGeoList = mapFunction(departureGeoList)
-    const parsedArrivalGeoList = mapFunction(arrivalGeoList)
-
-    const onChangeSelect = (event) => {
-
-        const id = event.target.value
-        
-        if (event.target.name === "departure-select") {
-            const myLocation = departureGeoList.find((loc)=> loc.id == id)
-            console.log({myLocation})
-            setSelectedDepartureLocation({
-                departureName: myLocation.name, 
-                departureLatitude: myLocation.latitude,
-                departureLongitude: myLocation.longitude
-                
-            })
-
-        }
-        if (event.target.name === "arrival-select") {
-            const myLocation = arrivalGeoList.find((loc)=> loc.id == id)
-            console.log({myLocation})
-            setSelectedArrivalLocation({
-                arrivalName: myLocation.name,
-                arrivalLatitude: myLocation.latitude,
-                arrivalLongitude: myLocation.longitude
-            })
-        }
-
-    }
-    
-    const onChange = (event) => {
-        const newSearch = Object.assign({}, search)
-        newSearch[event.target.name] = event.target.value
-
-        if(event.target.name === "save") {
-            setSaveSearchChecked(!saveSearchChecked)
-        }
-
-
-        console.log("dep select: ",selectedDepartureLocation)
-        console.log("arr select: ",selectedArrivalLocation)
-
-        if (event.target.name === "departureString" && event.target.value.length >= 2) {
-            setDepartureGeoList([])
-        }
-
-        if (event.target.name === "arrivalString" && event.target.value.length >= 2) {
-            setArrivalGeoList([])
-        }
-
-        if (event.target.name === "departureString" && event.target.value.length > 2) {
-            const newGeoList = ExternalServices.getGeoList(event.target.value)
-            newGeoList.then(resultofGetGeoList => setDepartureGeoList(resultofGetGeoList))
-            newSearch["departureLatitude"] = event.target.latitude
-            newSearch["departureLongitude"] = event.target.longitude
-        }
-        if (event.target.name === "arrivalString" && event.target.value.length > 2) {
-            const newGeoList = ExternalServices.getGeoList(event.target.value)
-            newGeoList.then(resultofGetGeoList => setArrivalGeoList(resultofGetGeoList))
-            newSearch["arrivalLatitude"] = event.target.latitude
-            newSearch["arrivalLongitude"] = event.target.longitude
-        }
-        setSearch(newSearch)
-    }
-
-
-
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-
-        const newGeoObj = {
-            departureName: selectedDepartureLocation.departureName,
-            departureDate: search.departureDate,
-            departureTime: search.departureTime,
-            departureLongitude: selectedDepartureLocation.departureLongitude,
-            departureLatitude: selectedDepartureLocation.departureLatitude,
-            arrivalName: selectedArrivalLocation.arrivalName ? selectedArrivalLocation.arrivalName : "",
-            arrivalDate: search.arrivalDate,
-            arrivalTime: search.arrivalTime,
-            arrivalLatitude: selectedArrivalLocation.arrivalLatitude,
-            arrivalLongitude: selectedArrivalLocation.arrivalLongitude
-        }
-        setGeoObj(newGeoObj)
-
-        if (saveSearchChecked === true) {
-            postFlight(newGeoObj)
-            .then( () => {
-                
-                getFlights().then((returnedFlights) => {
-                setSavedSearchList(returnedFlights)})
-
-            })
-        }
-
-        setSearch({
-            departureString: "",
-            departureDate: "",
-            departureTime: "",
-            arrivalString: "",
-            arrivalDate: "",
-            arrivalTime: ""
-        })
-
-        setSaveSearchChecked(false)
-
-        setDepartureGeoList("")
-        setArrivalGeoList("")
-        setSelectedDepartureLocation({
-            departureLatitude: "",
-            departureLongitude: "",
-        })
-
-        setSelectedArrivalLocation({
-            arrivalLatitude: "",
-            arrivalLongitude: "",
-        })
-
-        runForecast(newGeoObj)
-    }
-    
-    return (
-        <form onSubmit={handleSubmit} className="search-form">
-            <div className="inform-container">
-            <div className="departure-container">
-                <label className="form-label" htmlFor="departure-name">From:</label>
-                <input className="form-input" list="departure_name" name="departureString" id="departure-name" value={search.departureString} onChange={onChange} placeholder="type here" required />
-                {parsedDepartureGeoList?                 
-                <select id="departure-select" className="search-select" name="departure-select" onChange={onChangeSelect} required >
-                    <option> Please select </option>
-                    {parsedDepartureGeoList}
-                </select>
-                :
-                <select className="search-select"><option>Nothing Found</option></select>}
-                <label className="form-label" htmlFor="departure-date">Date:</label>
-                <input className="form-input" type="date" id="departure-date" name="departureDate" value={search.departureDate} onChange={onChange} min={rightHereRightNow} required />
-                <label className="form-label" htmlFor="departure-time">Time:</label>
-                <input className="form-input" type="time" id="departure-time" name="departureTime" value={search.departureTime} onChange={onChange}  required />
-            </div>
-            <div className="arrival-container">
-            <label className="form-label" htmlFor="arrival-name">To:</label>
-                <input className="form-input" list="arrival_name"id="arrival-name" name="arrivalString" value={search.arrivalString} onChange={onChange} placeholder="type here" required />
-                {parsedArrivalGeoList?                 
-                <select id="arrival-select" className="search-select" name="arrival-select" onChange={onChangeSelect} required >
-                    <option> Please select </option>
-                    {parsedArrivalGeoList}
-                </select>
-                :
-                <select className="search-select"><option>Nothing Found</option></select>}
-                <label className="form-label" htmlFor="arrival-date">Date:</label>
-                <input className="form-input" type="date" id="arrival-date" name="arrivalDate" value={search.arrivalDate} onChange={onChange} min={ search.departureDate || rightHereRightNow} required />
-                <label className="form-label" htmlFor="arrival-time">Time:</label>
-                <input className="form-input" type="time" id="arrival-time" name="arrivalTime" value={search.arrivalTime} onChange={onChange} required />
-            </div>
-            </div>
-            <div className="submit-save-container">
-            <div className="submit-container">
-                <input className="form-button" type="submit" value="Aerosure?"/>
-            </div>
-            <div className="form-save">
-                <label htmlFor="save-search">Save</label>
-                <input id="save-search" name="save" type="checkbox" value={saveSearchChecked} checked={saveSearchChecked} onChange={onChange}></input>
-            </div>
-
-            </div>
-        </form>
-    )
-}
-
-
-
-
-export default FormContainer
-
-const ArtSearch = ({getArt, setPage, page, setQuery, setCategory, query, category}) => {
-
-    const handleSelectChange = (event) => {
-        setCategory((event.target.value).toLowerCase())
-    }
-
-    const handleQueryChange = (event) => {
-        setQuery((event.target.value).toLowerCase())
+    const handleTotalDotChange = (event) => {
+        setTotalDots(event.target.value)
     };
-
+    const handleCanvasHeightChange = (event) => {
+        setCanvasHeight(event.target.value)
+    };
+    const handleCanvasWidthChange = (event) => {
+        setCanvasWidth(event.target.value)
+    };
+    const handleBrushStrokeLengthChange = (event) => {
+        setBrushStrokeLength(event.target.value)
+    };
+    const handleDiameterChange = (event) => {
+        setDiameter(event.target.value)
+    };
+    const handleOpacityChange = (event) => {
+        setOpacity(event.target.value)
+    };
+    const handleDirectionBeforeChange = (event) => {
+        setDirectionBeforeChange(event.target.value)
+    };
+    const handleSpeedChange = (event) => {
+        setSpeed(event.target.value)
+    };
     const handleSubmit = (event) => {
         event.preventDefault();
-        if (page !== 1){
-            setPage(1)
-        }
-        if (query && category) {
-            getArt(query, category, page)
-        }
-    };
+        let runAgain = [...run]
+        setRun([runAgain[0] + 1]);
+    }
 
     return (
-        <>
+        <div>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="category">
-                    Choose a category:
-                    <select id ="category" onChange={handleSelectChange} required>
-                        <option value="q">Any</option>
-                        <option value="query[term][artist_title]">Artist</option>
-                        <option value="query[term][subject_titles]">Subject</option>
-                        <option value="query[term][title]">Title</option>
-                        <option value="query[term][style_titles]">Style</option>
-                        <option value="query[term][theme_titles]">Theme</option>
-                    </select>
+                <label>Total # of Dots:
+                    <input onChange={handleTotalDotChange} min={1000} value={totalDots} type='number'></input>
                 </label>
-                <label>Enter your search query:
-                    <input id="search" type="text" value={query} onChange={handleQueryChange} required/>
+                <label>Brush Stroke Length:
+                    <input onChange={handleBrushStrokeLengthChange} value={brushStrokeLength} type='number'></input>
                 </label>
-                <input type="submit" value="Search" />
+                <label>Change Direction (px):
+                    <input onChange={handleDirectionBeforeChange} min={1} value={directionBeforeChange} type='number'></input>
+                </label>
+                <label>Diameter (px):
+                    <input onChange={handleDiameterChange} value={diameter} type='number'></input>
+                </label>
+                <label>Opacity (%):
+                    <input onChange={handleOpacityChange} min={0} max={100} value={opacity} type='number'></input>
+                </label>
+                <label>Canvas Height (pixels):
+                    <input onChange={handleCanvasHeightChange} min={100} max={700} value={canvasHeight} type='number'></input>
+                </label>
+                <label>Canvas Width (pixels):
+                    <input onChange={handleCanvasWidthChange} min={100} max={1450} value={canvasWidth} type='number'></input>
+                </label>
+                <label>Speed of dot generation (ms):
+                    <input onChange={handleSpeedChange} min={0.1} max={1000} value={speed} type='number' step={0.1}></input>
+                </label>
+                <input type="submit" value="ART!" />
             </form>
-        </>
+        </div>
     );
-
 };
 
-export default ArtSearch;
+export default ParameterForm;
